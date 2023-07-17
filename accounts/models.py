e319ef440278de2f9c3b82a02445afcec2442fd6
Abdulrahman_Elsharef <78035855+AbdulrahmanElsharef@users.noreply.github.com>
2023-07-18 01:07:24 +0300

    add user type to profile models

--- accounts/models.py
+++ accounts/models.py
@@ -5,12 +5,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext as _  # Create your models here.
 
-
+USER_TYPE=(('Employee','Employee'),('Employer','Employer'))
 class Profile(models.Model):
     user = models.OneToOneField(
         User, related_name='user_profile', on_delete=models.CASCADE)
     image = models.ImageField(
         upload_to='profile', null=True, blank=True, default='default.png')
+    user_type=models.CharField(_("user type"), max_length=50,choices=USER_TYPE)
     code = models.CharField(max_length=8, default=generate_code)
     job = models.CharField(max_length=50)
     linkedin = models.URLField(_("linkedin"), max_length=100)
